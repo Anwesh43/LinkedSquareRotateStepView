400bf6efe01164a4a589b43c81d0815358922de9
Anwesh43 <anweshthecool0@gmail.com>
2018-11-05 02:13:39 +0530

    adding Float extension methods

--- squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
+++ squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
@@ -15,3 +15,11 @@ import android.graphics.Color
 val nodes : Int = 5
 val lines : Int = 4
 val color : Int = Color.parseColor("#673AB7")
+val scGap : Float = 0.05f
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.scaleFactor() : Float = Math.floor(this / 0.5).toFloat()
+
+fun Float.updateScale(dir : Float) : Float = dir * scGap * ((1 - scaleFactor())/lines + scaleFactor())
+
