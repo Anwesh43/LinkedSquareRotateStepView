72ece9eb8d896bf41fb30f001e1b1ba352e5fd65
Anwesh43 <anweshthecool0@gmail.com>
2018-11-05 02:09:20 +0530

    adding libraries and defining constants

--- squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
+++ squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
@@ -3,3 +3,15 @@ package com.anwesh.uiprojects.squarerotatestepview
 /**
  * Created by anweshmishra on 05/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val color : Int = Color.parseColor("#673AB7")
