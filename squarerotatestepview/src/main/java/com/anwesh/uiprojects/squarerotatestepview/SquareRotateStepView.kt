63f613d722602924e0fc2138213c5f2777b624e7
Anwesh43 <anweshthecool0@gmail.com>
2018-11-05 02:42:18 +0530

    adding SquareRotateStep class

--- squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
+++ squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
@@ -179,4 +179,28 @@ class SquareRotateStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareRotateStep(var i : Int) {
+
+        private var curr : SRSNode = SRSNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
