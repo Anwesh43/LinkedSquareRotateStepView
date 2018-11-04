68cd2260a57369fffb8e5dd06bdbcdeaf3714993
Anwesh43 <anweshthecool0@gmail.com>
2018-11-05 02:30:03 +0530

    adding SquareRotateStepView class

--- squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
+++ squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
@@ -66,3 +66,21 @@ fun Canvas.drawSRSNode(i : Int, scale : Float, paint : Paint) {
         }
     }
 }
+
+class SquareRotateStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+        
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
