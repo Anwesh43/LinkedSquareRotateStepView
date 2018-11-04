0a5f05fc12c06a633f59764b2769133cb2ded546
Anwesh43 <anweshthecool0@gmail.com>
2018-11-05 02:27:34 +0530

    adding drawSRSNode canvas extension method

--- squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
+++ squarerotatestepview/src/main/java/com/anwesh/uiprojects/squarerotatestepview/SquareRotateStepView.kt
@@ -11,11 +11,14 @@ import android.app.Activity
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Color
+import android.graphics.PointF
 
 val nodes : Int = 5
 val lines : Int = 4
 val color : Int = Color.parseColor("#673AB7")
 val scGap : Float = 0.05f
+val sizeFactor : Int = 3
+val strokeFactor : Float = 60f
 
 fun Int.getInverse() : Float = 1f / this
 
@@ -23,3 +26,43 @@ fun Float.scaleFactor() : Float = Math.floor(this / 0.5).toFloat()
 
 fun Float.updateScale(dir : Float) : Float = dir * scGap * ((1 - scaleFactor())/lines + scaleFactor())
 
+fun Float.divideScale(n : Int, i : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Canvas.getGapSizePoint(n : Int, sf : Int) : PointF {
+    val gap : Float = width.toFloat() / (n + 1)
+    return PointF(gap, gap / sf)
+}
+
+fun Canvas.drawInMiddle(x : Float, cb : () -> Unit ) {
+    save()
+    translate(x, height.toFloat()/2)
+    cb()
+    restore()
+}
+
+fun Canvas.setPaintProps(paint : Paint, color : Int, strokeFactor : Float) {
+    paint.strokeWidth = Math.min(width, height) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = color
+}
+
+fun Canvas.drawSRSNode(i : Int, scale : Float, paint : Paint) {
+    val gapSizePoint : PointF = getGapSizePoint(nodes, sizeFactor)
+    val gap : Float = gapSizePoint.x
+    val size : Float = gapSizePoint.y
+    val deg : Float = 360f / lines
+    setPaintProps(paint, color, strokeFactor)
+    drawInMiddle(gap * (i + 1)) {
+        val sc1 : Float = scale.divideScale(2, 0)
+        val sc2 : Float = scale.divideScale(2, 1)
+        rotate(90f * sc2)
+        for (j in 0..(lines - 1)) {
+            val sc : Float = sc1.divideScale(lines, j)
+            save()
+            rotate(deg * j)
+            translate(0f, size/2)
+            drawLine(size, 0f, size - 2 * size * sc, 0f, paint)
+            restore()
+        }
+    }
+}
